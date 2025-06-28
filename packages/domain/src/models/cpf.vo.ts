5998221b44e2990c1853091356475d8d507dafec
Felipe Barros <felipejonathan.dev@gmail.com>
2025-06-28 19:20:34 -0300

    resource(domain): criando objeto de valor cpf e seus testes

---
+++ packages/domain/src/models/cpf.vo.ts
@@ -0,0 +1,28 @@
+import DomainError from "./_error/domain.error";
+
+export default class CPF {
+    readonly value: string;
+
+    constructor(value: string) {
+        if (!CPF.isValid(value))
+            DomainError.launch(
+                'cpf.invalid',
+                value,
+                { message: 'CPF inválido.' }
+            )
+
+        this.value = value;
+    }
+
+    get formatted(): string {
+        return this.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    }
+
+    static isValid(cpf: string): boolean {
+        const cleaned = cpf.replace(/\D/g, '');
+        if (cleaned.length !== 11 || /^(\d)\1{10}$/.test(cleaned)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ packages/domain/test/models/cpf.vo.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from '@jest/globals'
+import CPF from '../../src/models/cpf.vo'
+import DomainError from '../../src/models/_error/domain.error';
+
+describe("Testes para o objeto de valor: CPF", () => {
+    it("Deve criar um CPF válido", () => {
+        const cpf = new CPF("12345678909");
+        expect(cpf.value).toBe("12345678909");
+        expect(cpf.formatted).toBe("123.456.789-09");
+    });
+
+    it("Deve lançar erro para CPF inválido", () => {
+        expect(() => new CPF("1234567890")).toThrow(DomainError);
+        expect(() => new CPF("123456789012")).toThrow(DomainError);
+        expect(() => new CPF("11111111111")).toThrow(DomainError);
+    });
+
+    it("Deve validar corretamente CPFs", () => {
+        expect(CPF.isValid("12345678909")).toBe(true);
+        expect(CPF.isValid("11111111111")).toBe(false);
+        expect(CPF.isValid("1234567890")).toBe(false);
+    });
+}) 
